b54d4e9eef647badb3f4e42bd11eabce28a7c500
EldraPratama <76528642+EldraPratama@users.noreply.github.com>
2023-02-11 09:42:06 +0700

    1 file modified

--- HalamanHome/HalamanHome.js
+++ HalamanHome/HalamanHome.js
@@ -42,8 +42,80 @@ class HalamanHome extends React.Component {
     return (
       <div className="col-md-12 col-md-offset-3">
         <div className="row justify-content-center mt-5">
-          <div className="col-md-4 mt-5">
-            <div className="card shadow mb-6 bg-body rounded mt-5">
+          <div className="col-md-5">
+            <div
+              class="card shadow mb-5"
+              style={{ 'background-color': '#d8d57c' }}
+            >
+              <div className="row">
+                <div className="col-md-3">
+                  <div class="card-body">
+                    <i
+                      class="bi bi-person-circle"
+                      style={{ 'font-size': '3em', 'margin-left': '20px' }}
+                    ></i>
+                  </div>
+                </div>
+                <div className="col-md-9">
+                  <div class="card-body">Name : Eldra Surya Pratama</div>
+                  <div class="card-body">Position : Penjaga Toko</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-6">
+                <div
+                  class="card shadow"
+                  style={{ 'background-color': '#92d4df', height: '80px' }}
+                >
+                  <div class="card-body text-center">Absen Masuk</div>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div
+                  class="card shadow"
+                  style={{ 'background-color': '#87d266', height: '80px' }}
+                >
+                  <div class="card-body text-center">Absen Keluar</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="row mt-5">
+              <div className="col-md-6">
+                <div
+                  class="card shadow"
+                  style={{ 'background-color': '#d26c66' }}
+                >
+                  <div class="card-header text-center">Izin</div>
+                  <div class="card-body text-center">
+                    Isi Form terlebih dahulu untuk mengajukan izin kerja
+                    <div className="card rounded-pill text-red mt-5">
+                      <div className="card-body">Ajukan Izin</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div
+                  class="card shadow"
+                  style={{ 'background-color': '#dc92df' }}
+                >
+                  <div class="card-header text-center font-weight-bold">
+                    Sakit
+                  </div>
+                  <div class="card-body text-center">
+                    Isi Form terlebih dahulu untuk mengajukan sakit kerja
+                    <div className="card rounded-pill text-red mt-5">
+                      <div className="card-body">Ajukan Sakit</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* <div className="card shadow mb-6 bg-body rounded mt-5">
               <div className="card-header text-center">
                 <h2>Home</h2>
               </div>
@@ -90,14 +162,35 @@ class HalamanHome extends React.Component {
                   </div>
                 </form>
               </div>
-            </div>
+            </div> */}
             <nav
-              class="navbar fixed-bottom py-lg-4 col-md-5"
-              style={{ 'background-color': '#e3f2fd', 'margin-left': '400px' }}
+              class="navbar fixed-bottom py-lg-2 col-md-5"
+              style={{ 'background-color': '#e3f2fd', 'margin-left': '410px' }}
             >
               <div class="container-fluid">
+                <a class="navbar-brand mx-auto" href="home">
+                  <i
+                    class="bi bi-house-fill"
+                    style={{ 'font-size': '2em' }}
+                  ></i>
+                </a>
                 <a class="navbar-brand mx-auto" href="#">
-                  Fixed bottom
+                  <i
+                    class="bi bi-clock-history"
+                    style={{ 'font-size': '2em' }}
+                  ></i>
+                </a>
+                <a class="navbar-brand mx-auto" href="#">
+                  <i
+                    class="bi bi-megaphone-fill"
+                    style={{ 'font-size': '2em' }}
+                  ></i>
+                </a>
+                <a class="navbar-brand mx-auto" href="#">
+                  <i
+                    class="bi bi-person-fill"
+                    style={{ 'font-size': '2em' }}
+                  ></i>
                 </a>
               </div>
             </nav>
